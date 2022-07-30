3a20c69c97e5a8539745967fb6095338ff40ddf1
Glebosh <glebosh@gmail.com>
2022-07-30 11:30:13 +0300

    add simple commands from tutr

---
+++ main.py
@@ -0,0 +1,88 @@
+import telegram
+import logging
+from telegram import __version__ as tg_ver
+from telegram.ext import Updater
+
+TOKEN = '5484155734:AAELvAPIUJ7SSItY00aVn8226KFQZZrQJsE'
+
+bot = telegram.Bot(token=TOKEN)
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+
+#ERROR config
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+
+# start handler
+from telegram import Update
+from telegram.ext import CallbackContext
+
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, give me your money NOW!")
+
+from telegram.ext import CommandHandler
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+
+# echo handler
+from telegram.ext import MessageHandler, Filters
+
+def echo(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+
+#caps handler
+def caps(update: Update, context: CallbackContext):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+
+#inline
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+
+def inline_caps(update: Update, context: CallbackContext):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = []
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+from telegram.ext import InlineQueryHandler
+
+inline_caps_handler = InlineQueryHandler(inline_caps)
+dispatcher.add_handler(inline_caps_handler)
+
+
+# unkown commands
+def unknown(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
+
+#strat polling
+updater.start_polling()
+
+#stop polling
+# updater.stop()
+
+
+# print(bot.send_message(text='Привет, я робот, который хочет захватить мир!', chat_id='-705446959'))
